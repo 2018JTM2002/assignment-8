d8f7b67080de50690902d1d1a787728a2ad5aedc
2018JTM2002 <royshivaji13@gmail.com>
2018-09-27 14:34:25 +0530

    i have made canges for the first time

--- ps1.py
+++ ps1.py
@@ -1,3 +1,18 @@
 ###### this is the first .py file ###########
 
 ####### write your code here ##########
+
+print("no of rows and no of columns")
+n=int(input("value of the row"))
+m=int(input("value of the column"))
+print(n,m)
+
+lis = []
+
+for i in range(n):
+	s=list(input())
+	lis+=[s]
+
+print(lis)
+
+for
