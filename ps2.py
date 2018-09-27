34b0a4a46458d59b80f5fb4cee4c27270c653341
2018JTM2002 <royshivaji13@gmail.com>
2018-09-27 15:03:23 +0530

    i have made changes

--- ps2.py
+++ ps2.py
@@ -1,3 +1,35 @@
 ###### this is the second .py file ###########
 
 ####### write your code here ##########
+
+# Python 3.x code to demonstrate star pattern 
+  
+
+def numpat(n): 
+      
+    # initialising starting number  
+    num = 1
+  
+    # outer loop to handle number of rows 
+    for i in range(0, n): 
+      
+        # re assigning num 
+        num = 1
+      
+        # inner loop to handle number of columns 
+            # values changing acc. to outer loop 
+        for j in range(0, i+1): 
+          
+                # printing number 
+            print(num, end=" ") 
+          
+            # incrementing number at each column 
+            num = num + 1
+      
+        # ending line after each row 
+        print("\r") 
+  
+# Driver code 
+n = 5
+numpat(n) 
+
